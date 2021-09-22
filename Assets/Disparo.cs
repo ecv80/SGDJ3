44c1ada982b1c3eec800c6d06072ef01e12cdfe1
ecv <ecv@ecv.ecv>
2021-09-23 01:19:31 +0200

    disparo gancho hecho. Dejo el pooling para el final si da tiempo

--- Assets/Disparo.cs
+++ Assets/Disparo.cs
@@ -6,6 +6,9 @@ public class Disparo : MonoBehaviour
 {
     public GameObject eslabon=null;
 
+    bool disparando=false;
+    int disparo=0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,27 +19,68 @@ public class Disparo : MonoBehaviour
         Vector2 maxCoords=new Vector2(9f,5f);
         Vector2 minCoords=new Vector2(-9f,-5f);
 
-        Vector2 pos=minCoords;
-        Vector2 angulo=(maxCoords-minCoords).normalized;
-        Vector2 incremento=angulo*eslabon.transform.localScale.x;
-
-        int contador=0;
-
-        while (pos.x<maxCoords.x || pos.y<minCoords.y) {
-                
-            Instantiate<GameObject>(eslabon, pos, Quaternion.identity);
-
-            contador++;
-
-            pos+=incremento;
-        }
-
-        print(contador+" eslabones.");
+        StartCoroutine(Dispara(minCoords, maxCoords));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (disparando==false)
+            switch(disparo) {
+                case 0:
+                    StartCoroutine(Dispara(new Vector2(-9f, -5f), new Vector2(9f, -5f)));
+                    disparo++;
+                    break;
+                case 1:
+                    StartCoroutine(Dispara(new Vector2(-9f, -5f), new Vector2(-9f, 5f)));
+                    disparo++;
+                    break;
+                case 2:
+                    StartCoroutine(Dispara(new Vector2(9f, 5f), new Vector2(-9f, 5f)));
+                    disparo++;
+                    break;
+                default:
+                    break;
+            }
         
     }
+
+    IEnumerator Dispara (Vector2 origen, Vector2 destino, int eslabonesPorFrame=10) {
+        if (disparando) //ignorar
+            yield break;
+        disparando=true;
+
+        Vector2 pos=origen;
+        Vector2 angulo=(destino-origen).normalized;
+        Vector2 incremento=angulo*eslabon.transform.localScale.x;
+
+        // int contador=0;
+
+        int epf=eslabonesPorFrame;
+
+        while ((destino-pos).magnitude>incremento.magnitude) {
+                
+            Instantiate<GameObject>(eslabon, pos, Quaternion.identity);
+
+            // contador++;
+
+            pos+=incremento;
+            epf--;
+
+            // yield return new WaitForSeconds(cadencia);
+
+            if (epf<=0) {
+                epf=eslabonesPorFrame;
+                yield return 0;
+            }
+        }
+
+        // print(contador+" eslabones.");
+
+        disparando=false;
+        yield break;
+    }
+
+    
 }
