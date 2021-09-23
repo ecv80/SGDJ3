8551572b6662723e40202c2dd03acebb52bc6277
ecv <ecv@ecv.ecv>
2021-09-23 20:34:18 +0200

    cam follow

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,27 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject prota;
     public GameObject plataforma;
+
+    float camZ;
+
+    float plataformaMasAlta=-5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        camZ=transform.position.z;
+
         //Crea plataformas
-        float halfAncho=plataforma.transform.localScale.x/2f;
-        float halfAlto=plataforma.transform.localScale.y/2f;
-
-        for (int i=0; i<10; i++) {
-            Vector2 pos=new Vector2(Random.Range(-9f+halfAncho, 9f-halfAncho), Random.Range(-3.75f+halfAlto, 5f-halfAlto));
-            GameObject plat=Instantiate<GameObject>(plataforma, pos, Quaternion.identity);
-            List<Collider2D> colliders=new List<Collider2D>();
-            int plts=plat.GetComponent<Collider2D>().OverlapCollider(new ContactFilter2D().NoFilter(), colliders);
-            for (int j=0; j<plts; j++)
-                if (colliders[j].tag=="Plataforma")
-                    Destroy(colliders[j].gameObject);
-
-            //No vamos a reinstanciar indefinidamente las plataformas hasta dar con lugares donde no solapen a otras
-            //porque así tendremos más variabilidad a costa de ningún recurso más
-        }
+        AnadePlataformas(20, -5f);
         
     }
 
@@ -32,5 +25,34 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void LateUpdate() {
+
+        //Seguir al prota
+        Vector3 destino=new Vector3(prota.transform.position.x, prota.transform.position.y+4.5f, camZ); 
+        transform.position+=(destino-transform.position)*Time.deltaTime*3f;
+    }
+
+    void AnadePlataformas (int cantidad, float base_, float altura=20f) {
+        float halfAncho=plataforma.transform.localScale.x/2f;
+        float halfAlto=plataforma.transform.localScale.y/2f;
+
+        for (int i=0; i<cantidad; i++) {
+            Vector2 pos=new Vector2(Random.Range(-9f+halfAncho, 9f-halfAncho), Random.Range(base_+1.25f+halfAlto, altura-halfAlto));
+            GameObject plat=Instantiate<GameObject>(plataforma, pos, Quaternion.identity);
+            List<Collider2D> colliders=new List<Collider2D>();
+            int plts=plat.GetComponent<Collider2D>().OverlapCollider(new ContactFilter2D().NoFilter(), colliders);
+            for (int j=0; j<plts; j++)
+                if (colliders[j].tag=="Plataforma") {
+                    Destroy(plat);
+                    return;
+                }
+            if (pos.y>plataformaMasAlta)
+                plataformaMasAlta=pos.y;
+
+            //No vamos a reinstanciar indefinidamente las plataformas hasta dar con lugares donde no solapen a otras
+            //porque así tendremos más variabilidad a costa de ningún recurso más
+        }
     }
 }
