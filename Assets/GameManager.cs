2eea3a7f590b5983cef5a2ea198d2f6df6b1c08a
ecv <ecv@ecv.ecv>
2021-09-25 01:53:16 +0200

    siembra de plataformas mejorada

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
     float camZ;
 
     float plataformaMasAlta=-5f;
+    Vector2 plataformaMasDcha=new Vector2(20f, -5f);
+    Vector2 plataformaMasIzqda=new Vector2(-20f, -5f);
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,7 @@ public class GameManager : MonoBehaviour
         camZ=transform.position.z;
 
         //Crea plataformas
-        AnadePlataformas(10, -5f);
+        AnadePlataformas(20, prota.transform.position);
         
     }
 
@@ -35,13 +37,26 @@ public class GameManager : MonoBehaviour
 
         //Añadir plataformas si eso
         if (transform.position.y>plataformaMasAlta-10f) { //mejor 10 que 5, por dar más margen
-            AnadePlataformas(10, transform.position.y+5f);
+            AnadePlataformas(20, new Vector2(transform.position.x, transform.position.y+5f));
             //Si se atraganta, la convertimos en corutina luego
-            //pero antes ver que tal funciona con pooling
+            //pero antes a ver que tal funciona con pooling
         }
+
+        //Resetear plataformas mas a la dcha e izqda, segun se actualiza la altura de la camara
+        if (plataformaMasDcha.y<transform.position.y-5f)
+            plataformaMasDcha=transform.position;
+        if (plataformaMasIzqda.y<transform.position.y-5f)
+            plataformaMasIzqda=transform.position;
+
+        //Añadir plataformas a los lados si eso
+        if (transform.position.x>plataformaMasDcha.x-20f)
+            AnadePlataformas(20, new Vector2(transform.position.x+30f, transform.position.y-5f));
+        if (transform.position.x<plataformaMasIzqda.x+20f)
+            AnadePlataformas(20, new Vector2(transform.position.x-30f, transform.position.y-5f));
+
     }
 
-    void AnadePlataformas (int cantidadPorTramo, float base_, int tramos=2) {
+    void AnadePlataformas (int cantidadPorTramo, Vector2 origen, int tramos=2) {
         float halfAncho=plataforma.transform.localScale.x/2f;
         float halfAlto=plataforma.transform.localScale.y/2f;
 
@@ -49,7 +64,7 @@ public class GameManager : MonoBehaviour
         //en pantalla en todo momento, por lo que voy a partir la instanciación en trozos de a 10.
         for (int tramo=0; tramo<tramos; tramo++) {
             for (int i=0; i<cantidadPorTramo; i++) {
-                Vector2 pos=new Vector2(Random.Range(prota.transform.position.x-9f+halfAncho, prota.transform.position.x+9f-halfAncho), Random.Range(base_+tramo*10f+1.25f+halfAlto, base_+(tramo+1)*10f-halfAlto));
+                Vector2 pos=new Vector2(Random.Range(origen.x-20f+halfAncho, origen.x+20f-halfAncho), Random.Range(origen.y+tramo*10f+1.25f+halfAlto, origen.y+(tramo+1)*10f-halfAlto));
                 GameObject plat=Instantiate<GameObject>(plataforma, pos, Quaternion.identity);
                 List<Collider2D> colliders=new List<Collider2D>();
                 int plts=plat.GetComponent<Collider2D>().OverlapCollider(new ContactFilter2D().NoFilter(), colliders);
@@ -60,10 +75,16 @@ public class GameManager : MonoBehaviour
                     }
                 if (pos.y>plataformaMasAlta)
                     plataformaMasAlta=pos.y;
+                if (pos.x>plataformaMasDcha.x)
+                    plataformaMasDcha=pos;
+                if (pos.x<plataformaMasIzqda.x)
+                    plataformaMasIzqda=pos;
+                    
 
                 //No vamos a reinstanciar indefinidamente las plataformas hasta dar con lugares donde no solapen a otras
                 //porque así tendremos más variabilidad a costa de ningún recurso más
             }
         }
     }
+
 }
