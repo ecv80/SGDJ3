ea10558f1bd77c2f59ceefe9941d87b0f4c25b54
ecv <ecv@ecv.ecv>
2021-09-25 16:15:45 +0200

    lanzamiento basico de cocos

---
+++ Assets/Coco.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coco : MonoBehaviour
+{
+    public Vector3 destino=Vector3.zero;
+    public float velocidad=1f;
+
+    float nacimiento=0f;
+    float expectativaVida=100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nacimiento=Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time>nacimiento+expectativaVida) { //Has vivido demasiado, hamijo
+            Destroy(gameObject);
+            return;
+        }
+
+        //Vas p'ande t'an dicho y punto
+        transform.position+=(destino-transform.position).normalized*Time.deltaTime*velocidad;
+    }
+}
--- Assets/MonoCabron.cs
+++ Assets/MonoCabron.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MonoCabron : MonoBehaviour
 {
+    public GameObject coco;
+
+    float ultimoCocoT;
+    float cadenciaTiradaCocos=3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ultimoCocoT=Time.time;
     }
 
     // Update is called once per frame
@@ -19,7 +24,14 @@ public class MonoCabron : MonoBehaviour
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag=="Prota") {//A hacer el cabrón lanzando cocos
-            print("COCAZO");
+            if (Time.time>ultimoCocoT+cadenciaTiradaCocos) {//Han pasado mas de cadenciaTiradaCocos segundos desde la ultima tirada de cocos?
+                ultimoCocoT=Time.time;
+
+                GameObject cocoInstance=Instantiate<GameObject>(coco, transform.position, Quaternion.identity);
+                Coco c=cocoInstance.GetComponent<Coco>();
+                c.destino=GameManager.instancia.prota.transform.position; //Somewhere over the rainbow
+                c.velocidad=5f;
+            }
         }
     }
 
